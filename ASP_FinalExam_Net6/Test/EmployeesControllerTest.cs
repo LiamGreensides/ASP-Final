9bf028eff53fc22683f8a293d49ce9bf3ef86e5c
LiamGreensides <liamgreensides61@gmail.com>
2022-08-19 16:42:20 -0400

    Added unit test
    unit test to test if index returns employee list

---
+++ ASP_FinalExam_Net6/Test/EmployeesControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using ASP_FinalExam_Net6.Controllers;
+using ASP_FinalExam_Net6.Data;
+using ASP_FinalExam_Net6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+namespace ASP_FinalExam_Net6.Test
+{
+    public class EmployeesControllerTest
+    {
+        [Fact]
+        public async Task Index_Returns_ViewResult_And_EmployeeList()
+        {
+            using (var testDb = new ApplicationDbContext(this.GetTestDbOpts()))
+            {
+                var testCtrl = new EmployeesController(testDb);
+                var result = await testCtrl.Index();
+                var resVr = Assert.IsType<ViewResult>(result);
+                Assert.IsAssignableFrom<IEnumerable<Employee>>(resVr.ViewData.Model);
+            }
+        }
+
+
+        private DbContextOptions<ApplicationDbContext> GetTestDbOpts()
+        {
+            var opts = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "TestEmployeeCntrllr").Options;
+            return opts;
+        }
+
+    }
+
+    
+}
