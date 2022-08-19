4fcbd359ef4746e97cbf62f59948f319d596bd07
LiamGreensides <liamgreensides61@gmail.com>
2022-08-19 15:53:11 -0400

    Models Created
    Created both models with one to one relationship and proper annotations

--- ASP_FinalExam_Net6/Models/Employee.cs
+++ ASP_FinalExam_Net6/Models/Employee.cs
@@ -1,21 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASP_FinalExam_Net6.Models
 {
-    public class Department
+    public class Employee
     {
-        [ForeignKey("Employee")]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Name is required.")]
         [MaxLength(100)]
         public string Name { get; set; }
 
-        [MinLength(0)]
-        public int EmployeeCount { get; set; }
+        public bool IsManager { get; set; }
 
-        public virtual Employee Employee { get; set; }
-            
+        public virtual Department Department { get; set; }
     }
 }
