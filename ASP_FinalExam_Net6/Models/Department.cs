721e5cc73f5499c09484262d15a5bb632acad384
LiamGreensides <liamgreensides61@gmail.com>
2022-08-19 15:24:21 -0400

    Department model created
    Created department model with the proper values and data annotation

---
+++ ASP_FinalExam_Net6/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ASP_FinalExam_Net6.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MinLength(0)]
+        public int EmployeeCount { get; set; }
+            
+    }
+}
